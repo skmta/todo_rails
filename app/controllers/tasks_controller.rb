c353f25f2b13649b94ffadfa852febc22e0c5c0c
skmta <sakamoto@eightbeat.co.jp>
2018-10-22 16:31:39 +0900

    投稿機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,34 @@
 class TasksController < ApplicationController
 
-    def index
-    end
+	  # トップページで投稿一覧を表示
+  	def index
+  		@tasks = Task.all.order(:due_date)
+  	end
 
+    #オブジェクトの生成、dbに反映されない
     def new
     end
 
+    #オブジェクトを生成、保存してインサートする
     def create
+    	@task = Task.new(create_task_params)
+    	if @task.save
+    		redirect_to tasks_path
+    	else
+    		render @task.error
+    	end
+    end
+
+    #投稿一覧を表示
+    def show
+    	@task = Task.all
+    	# @task = Task.find(params[:id])
+    end
+
+    # 
+    private
+    def create_task_params
+        params.permit(:title, :description, :due_date)
     end
 
 end
